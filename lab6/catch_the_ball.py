b76f8934081807a70aa97f444013c0d532fb2a31
AzizbekTursunov <tursunov.ag@phystech.edu>
2021-11-04 19:09:41 +0500

    I wrote some cod to thisfile

--- lab6/catch_the_ball.py
+++ lab6/catch_the_ball.py
@@ -25,6 +25,15 @@ wrong_clicks = 0
 
 
 def new_ball():
+    """
+    This function must created one new ball
+    :return: the function must return list with elements:
+    color ball
+    coordinate ball(axis x and axis y) [x, y]
+    radius ball
+    speed ball on axis x
+    speed ball on axis y
+    """
     global Number_all_balls
     Number_all_balls += 1
     color = random.choice([RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN])
@@ -37,6 +46,11 @@ def new_ball():
 
 
 def move_ball(list_parametr_of_new_ball):
+    """
+    In this function is written move law of the ball. This function changes list 'list_parametr_of_new_ball'.
+    :param list_parametr_of_new_ball: function new_ball() returned this list, then function move_ball taked this list
+    :return: None
+    """
     circle(sc, list_parametr_of_new_ball[0], list_parametr_of_new_ball[1], list_parametr_of_new_ball[2])
     if list_parametr_of_new_ball[1][0] >= 1000 - list_parametr_of_new_ball[2] or list_parametr_of_new_ball[1][0] <= \
             list_parametr_of_new_ball[2]:
@@ -49,6 +63,12 @@ def move_ball(list_parametr_of_new_ball):
 
 
 def click(event, list_parametr_of_new_ball):
+    """
+    This function checked, you are hit the ball or not
+    :param event: All events in pygame.event.get()
+    :param list_parametr_of_new_ball:  function new_ball() returned this list, then function click taked this list
+    :return: None
+    """
     global Your_score, wrong_clicks, hit_the_target
     ax = (list_parametr_of_new_ball[1][0] - event.pos[0]) ** 2
     by = (list_parametr_of_new_ball[1][1] - event.pos[1]) ** 2
@@ -69,19 +89,18 @@ finished = False
 n = 0
 # main loop
 while not finished:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event, list_parametr_of_new_ball)
-    clock.tick(FPS)
     if n % 50 == 0:
         list_parametr_of_new_ball = new_ball()
     else:
         move_ball(list_parametr_of_new_ball)
     n += 1
     pygame.display.update()
-    sc.fill('lightgrey')
-
-print('Number_all_balls : ', Number_all_balls, 'Total score : ', Your_score)
+    sc.fill('lightgray')
+print('Number_all_balls : ', Number_all_balls, 'Total score : ', Your_score, 'Total wrong clicks : ', wrong_clicks)
 pygame.quit()
